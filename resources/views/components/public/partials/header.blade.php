9c34c9d903c64ab0c3805c70904713ac2628907b
FournyAmandine <amandine.fourny@student.hepl.be>
2026-04-21 22:04:36 +0200

    Add css and html for the header

--- resources/views/components/public/partials/header.blade.php
+++ resources/views/components/public/partials/header.blade.php
@@ -1,3 +1,14 @@
-<header>
-    <h1 class="sro">{{$title}}</h1>
+<header class="header">
+    <h1 class="sro header__title">{{$title}}</h1>
+    <div class="header__imgContainer">
+        <img class="header__imgContainer__img" src="{{ asset('assets/img/logo-header.svg') }}" alt="Logo CareTogether">
+    </div>
+    <nav class="header__navContainer">
+        <x-public.navigation.list.links name_parent="header__navContainer"/>
+    </nav>
+    <x-public.utils.search name_parent="header"/>
+    <div class="header__buttons">
+        <x-public.utils.link name_parent="button--simple header__buttons" href="#" label="S'inscrire"/>
+        <x-public.utils.link name_parent="header__buttons" href="#" label="Se connecter"/>
+    </div>
 </header>
