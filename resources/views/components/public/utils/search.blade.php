868e649ff1e974ab7a20ebde2146ad32ff4b0e7e
FournyAmandine <amandine.fourny@student.hepl.be>
2026-04-21 22:03:21 +0200

    Add search component

---
+++ resources/views/components/public/utils/search.blade.php
@@ -0,0 +1,10 @@
+@props(['name_parent'])
+
+<form class="{!! $name_parent !!}__formSearch" action="{{ route('public.posts.index') }}" method="get">
+    <input class="{!! $name_parent !!}__formSearch__input" type="search" placeholder="Rechercher..." name="term" value="{{ request('term') }}">
+    <button type="submit" class="{!! $name_parent !!}__formSearch__button">
+        <svg class="{!! $name_parent !!}__formSearch__button__icon">
+            <use xlink:href="{{ asset('assets/img/sprite.svg#search-button') }}"></use>
+        </svg>
+    </button>
+</form>
