f47ab352dcf4e2c9a1f2d2d47e089460be2300b4
FournyAmandine <amandine.fourny@student.hepl.be>
2026-04-22 21:15:01 +0200

    Add fonts and change it in the files

--- resources/views/components/public/navigation/list/link.blade.php
+++ resources/views/components/public/navigation/list/link.blade.php
@@ -1,3 +1,3 @@
 @props(['name_parent', 'href', 'label'])
 
-<li class="{!! $name_parent !!}__item"><a class="{!! $name_parent !!}__item__link" title="Aller vers la page {!! $label !!}" href="{!! $href !!}">{!! $label !!}</a></li>
+<li class="{!! $name_parent !!}__item"><a class="button button--simple {!! $name_parent !!}__item__link" title="Aller vers la page {!! $label !!}" href="{!! $href !!}">{!! $label !!}</a></li>
