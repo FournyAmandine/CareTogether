3af0593ac9bd38eb6399664015b9fbc93b937b71
FournyAmandine <amandine.fourny@student.hepl.be>
2026-04-22 20:20:46 +0200

    Add small changes in the components

--- resources/views/components/public/partials/header.blade.php
+++ resources/views/components/public/partials/header.blade.php
@@ -2,11 +2,11 @@
     <h1 class="sro header__title">{{$title}}</h1>
     <x-public.utils.logo name_parent="header" />
     <nav class="header__navContainer">
-        <x-public.navigation.list.list-header name_parent="header__navContainer"/>
+        <x-public.navigation.list.list-header name_parent="header__navigation"/>
     </nav>
     <x-public.utils.search name_parent="header"/>
     <div class="header__buttons">
-        <x-public.utils.link name_parent="button--simple header__buttons" href="#" label="S'inscrire"/>
-        <x-public.utils.link name_parent="header__buttons" href="#" label="Se connecter"/>
+        <x-public.utils.link name_parent="button--simple header__buttons" title="Aller vers la page S'inscrire" href="#" label="S'inscrire"/>
+        <x-public.utils.link name_parent="header__buttons" title="Aller vers la page Se connecter" href="#" label="Se connecter"/>
     </div>
 </header>
