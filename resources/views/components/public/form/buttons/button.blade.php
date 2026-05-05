e25cb65fdcd13de01e0a5927684bde37e22f9cb4
FournyAmandine <amandine.fourny@student.hepl.be>
2026-05-05 10:32:03 +0200

    Make the contactpage.blade.php

--- resources/views/components/public/form/buttons/button.blade.php
+++ resources/views/components/public/form/buttons/button.blade.php
@@ -1,8 +1,11 @@
-@props(['class', 'text'])
+@props(['text', 'name_parent', 'class_button'])
 
-<div>
-    <button class="{!! $class !!}" type="submit">
+<div class="{!! $name_parent !!}__buttonContainer">
+    <button class="button {!! $class_button !!} {!! $name_parent !!}__buttonContainer__button" type="submit">
         {!! $text !!}
+        <svg class="{!! $name_parent !!}__buttonContainer__button__icon">
+            <use xlink:href="{{ asset('assets/img/svg/sprite.svg#arrow-button') }}"></use>
+        </svg>
     </button>
 </div>
 
