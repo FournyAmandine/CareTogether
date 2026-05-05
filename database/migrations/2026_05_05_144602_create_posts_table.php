28c9eb5f6875b0c2343c33e8059f28fd03cb1aa9
FournyAmandine <amandine.fourny@student.hepl.be>
2026-05-05 17:18:25 +0200

    Add post table

---
+++ database/migrations/2026_05_05_144602_create_posts_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('locality');
+            $table->string('state');
+            $table->integer('price');
+            $table->string('category');
+            $table->string('img_path')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
