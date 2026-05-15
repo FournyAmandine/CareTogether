26459c4048009c2929f42a109e1ea5cfcff6ed9b
FournyAmandine <amandine.fourny@student.hepl.be>
2026-05-15 21:40:17 +0200

    Add all the fill for the posts

---
+++ resources/views/pages/user/posts/⚡index/index.blade.php
@@ -0,0 +1,39 @@
+<main class="postsPage">
+    <x-user.utils.heading title="Vos annonces" :button="true"/>
+
+    <section class="posts">
+        <div class="wrapper wrapper--small">
+            <h3 class="posts__title">
+                Ventes/dons
+            </h3>
+            <div class="posts__listing">
+                @foreach($sales as $sale)
+                    <x-user.utils.post-card title="{{ $sale->name }}" type="{{ $sale->type }}"
+                                  svg="{!! Str::slug($sale->category, '_')!!}"
+                                  price="{{ $sale->price }}" locality="{{ $sale->locality }}"
+                                  state="{{ $sale->state }}" modifier="post"
+                                  imgSrc="{{ asset($sale->img_path) }}" src="{!! route('public.posts.show', $sale->id) !!}"
+                                    views="10" registered="10"/>
+                @endforeach
+            </div>
+        </div>
+    </section>
+
+    <section class="posts">
+        <div class="wrapper wrapper--small">
+            <h3 class="posts__title">
+                Locations/prêts
+            </h3>
+            <div class="posts__listing">
+                @foreach($rentals as $rental)
+                    <x-user.utils.post-card title="{{ $rental->name }}" type="{{ $rental->type }}"
+                                            svg="{!! Str::slug($rental->category, '_')!!}"
+                                            price="{{ $rental->price }}" locality="{{ $rental->locality }}"
+                                            state="{{ $rental->state }}" modifier="post"
+                                            imgSrc="{{ asset($rental->img_path) }}" src="{!! route('public.posts.show', $rental->id) !!}"
+                                            views="10" registered="10"/>
+                @endforeach
+            </div>
+        </div>
+    </section>
+</main>
