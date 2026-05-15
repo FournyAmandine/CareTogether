7a7eed2813285b7d1a356ddfb74af64a3133bd8d
FournyAmandine <amandine.fourny@student.hepl.be>
2026-05-15 14:42:42 +0200

    Add relations in the model

--- app/Models/User.php
+++ app/Models/User.php
@@ -51,4 +51,9 @@ class User extends Authenticatable
     {
         return $this->hasMany(Rental::class);
     }
+
+    public function sales(): HasMany
+    {
+        return $this->hasMany(Rental::class);
+    }
 }
