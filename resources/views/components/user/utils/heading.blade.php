6640f2046cab452cb3799181260b597cb0a0ca48
FournyAmandine <amandine.fourny@student.hepl.be>
2026-05-17 18:08:38 +0200

    Add a button in the heading

--- resources/views/components/user/utils/heading.blade.php
+++ resources/views/components/user/utils/heading.blade.php
@@ -1,4 +1,4 @@
-@props(['title'])
+@props(['title', 'button' => false])
 
 <section class="heading">
     <x-utils.deco modifier="user"/>
@@ -7,5 +7,13 @@
         <h2 class="maintitle maintitle--blue heading__title">
             {!! $title !!}
         </h2>
+        <div class="heading__actions">
+            @if($button)
+                <x-utils.link href="{!! route('user.posts.create') !!}"
+                              classButton="button button--red" label="Ajouter une annonce"
+                              title="Aller sur la page d'ajout d'une annonce" svg="add" name_parent="heading__actions"
+                />
+            @endif
+        </div>
     </div>
 </section>
