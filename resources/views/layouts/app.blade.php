198ad4f0172031fd974ea5314f562de9d310bba0
FournyAmandine <amandine.fourny@student.hepl.be>
2026-05-18 13:51:16 +0200

    Add jquery

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,6 +17,7 @@
 <livewire:user.partials.header/>
 {{ $slot }}
 {{--<livewire:widgets::modal />--}}
+<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 <script type="text/javascript" src="https://cdn.jsdelivr.net/gh/kenwheeler/slick@1.8.1/slick/slick.min.js"></script>
 </body>
 </html>
