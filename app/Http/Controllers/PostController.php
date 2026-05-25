40bf95923c50a38643fcce1ad1913707fb3e0bfb
FournyAmandine <amandine.fourny@student.hepl.be>
2026-05-25 13:14:13 +0200

    Change small details

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -8,15 +8,17 @@ class PostController extends Controller
 {
     public function index()
     {
-        $posts = Post::paginate(8);
+        $posts = Post::where('sold', 0)->paginate(8);
 
         return view('public.posts.index', compact('posts'));
     }
 
     public function show(Post $post)
     {
-        $posts = Post::where('posts.category_id', '=', $post->category->id)->where('posts.id', "!=", $post->id)->orderby('posts.created_at')->paginate(4);
+        $existingImages = $post->images->toArray();
 
-        return view('public.posts.show', compact('posts', 'post'));
+        $posts = Post::where('posts.category_id', '=', $post->category->id)->where('posts.id', "!=", $post->id)->where('sold', 0)->orderby('posts.created_at')->paginate(4);
+
+        return view('public.posts.show', compact('posts', 'post', 'existingImages'));
     }
 }
