b176b9027c3209098c6d9abb4e5913ce7d1cfc7e
FournyAmandine <amandine.fourny@student.hepl.be>
2026-05-27 10:45:31 +0200

    Add contact message table

---
+++ database/migrations/2026_05_26_200422_create_contact_messages_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateContactMessagesTable extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('contact_messages', function (Blueprint $table) {
+            $table->id();
+            $table->string('last_name');
+            $table->string('first_name');
+            $table->string('email');
+            $table->string('subject');
+            $table->tinyText('message');
+            $table->boolean('read')->default(0);
+            $table->foreignId('user_id')->nullable()->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('contact_messages');
+    }
+}
