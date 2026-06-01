7e64ec21c1651440307746dc66850a055dfc5a11
FournyAmandine <amandine.fourny@student.hepl.be>
2026-06-01 13:43:52 +0200

    Adding a default image

--- database/migrations/2026_05_14_105630_add_factors_in_users_table.php
+++ database/migrations/2026_05_14_105630_add_factors_in_users_table.php
@@ -9,7 +9,7 @@ class AddFactorsInUsersTable extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('profil_picture')->after('password')->nullable();
+            $table->string('profil_picture')->after('password')->nullable()->default('assets/img/header-profil.png');
             $table->string('role')->after('first_name')->default('Utilisateur');
             $table->string('tel')->after('profil_picture')->nullable();
             $table->string('address')->after('tel')->nullable();
