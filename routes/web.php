8601afe76099c9b55dfb0353be5c65ddea349bea
FournyAmandine <amandine.fourny@student.hepl.be>
2026-06-02 23:42:26 +0200

    Add route

--- routes/web.php
+++ routes/web.php
@@ -22,20 +22,20 @@ Route::get('/mentions', function (){ return view('public.mentionspage'); })->nam
 Route::get('/policy', function (){ return view('public.policypage'); })->name('public.policypage');
 Route::get('/conditions', function (){ return view('public.conditionspage'); })->name('public.conditionspage');
 
-/*Route::livewire('/admin/dashboard', 'pages::dashboard')->name('admin.dashboard')->middleware('auth', IsAdministrator::class);*/
+Route::livewire('/admin/dashboard', 'pages::admin.dashboard')->name('admin.dashboard')->middleware('auth', IsAdministrator::class);
 Route::livewire('/admin/messages', 'pages::admin.contact_messages.index')->name('admin.messages.index')->middleware('auth', IsAdministrator::class);
 
-Route::livewire('/user/dashboard', 'pages::user.dashboard')->name('user.dashboard')->middleware('auth');
-Route::livewire('/user/profil', 'pages::user.profil')->name('user.profil')->middleware('auth');
-Route::livewire('/user/messages{conversation?}', 'pages::user.messages')->name('user.messages')->middleware('auth');
+Route::livewire('/user/dashboard', 'pages::user.dashboard')->name('user.dashboard')->middleware('auth', IsUser::class);
+Route::livewire('/user/profil', 'pages::user.profil')->name('user.profil')->middleware('auth', IsUser::class);
+Route::livewire('/user/messages{conversation?}', 'pages::user.messages')->name('user.messages')->middleware('auth', IsUser::class);
 
-Route::livewire('/user/posts/create', 'pages::user.posts.create')->name('user.posts.create')->middleware('auth');
-Route::livewire('/user/posts/{post}/edit', 'pages::user.posts.edit')->name('user.posts.edit')->middleware('auth');
-Route::livewire('/user/posts/{post}', 'pages::user.posts.show')->name('user.posts.show')->middleware('auth');
-Route::livewire('/user/posts', 'pages::user.posts.index')->name('user.posts.index')->middleware('auth');
+Route::livewire('/user/posts/create', 'pages::user.posts.create')->name('user.posts.create')->middleware('auth', IsUser::class);
+Route::livewire('/user/posts/{post}/edit', 'pages::user.posts.edit')->name('user.posts.edit')->middleware('auth', IsUser::class);
+Route::livewire('/user/posts/{post}', 'pages::user.posts.show')->name('user.posts.show')->middleware('auth', IsUser::class);
+Route::livewire('/user/posts', 'pages::user.posts.index')->name('user.posts.index')->middleware('auth', IsUser::class);
 
-Route::livewire('/user/registered', 'pages::user.registered.index')->name('user.registered.index')->middleware('auth');
+Route::livewire('/user/registered', 'pages::user.registered.index')->name('user.registered.index')->middleware('auth', IsUser::class);
 
-Route::livewire('/user/sales', 'pages::user.sales.index')->name('user.sales.index')->middleware('auth');
+Route::livewire('/user/sales', 'pages::user.sales.index')->name('user.sales.index')->middleware('auth', IsUser::class);
 
-Route::livewire('/user/rentals', 'pages::user.rentals.index')->name('user.rentals.index')->middleware('auth');
+Route::livewire('/user/rentals', 'pages::user.rentals.index')->name('user.rentals.index')->middleware('auth', IsUser::class);
