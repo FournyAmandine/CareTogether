7b990daf5b482adde52f55bd881e3a3faf256a57
FournyAmandine <amandine.fourny@student.hepl.be>
2026-06-05 15:12:20 +0200

    Make the footer responsiv

--- resources/views/components/public/partials/footer.blade.php
+++ resources/views/components/public/partials/footer.blade.php
@@ -2,14 +2,16 @@
     <h2 class="sro footer__title">Footer</h2>
     <div class="footer__primary">
         <x-utils.logo name_parent="footer__primary" />
-        <div class="footer__primary__navigation">
-            <h3 class="maintitle maintitle--footer footer__primary__navigation__title">Navigation</h3>
-            <x-public.navigation.list.list-footer name_parent="footer__primary__navigation"/>
-        </div>
-        <div class="footer__primary__contact">
-            <h3 class="maintitle maintitle--footer footer__primary__contact__title">Contact</h3>
-            <x-utils.link name_parent="button--footer footer__primary__contact" href="mailto:amandine.fourny@student.hepl.be" label="amandine.fourny@student.hepl.be" title="Envoyer un mail à amandine.fourny@student.hepl.be"/>
-            <x-utils.link name_parent="button--footer footer__primary__contact" href="tel:0472483260" label="0472 48 32 60" title="Télephone au 0472 48 32 60"/>
+        <div class="footer__primary__links">
+            <div class="footer__primary__links__navigation">
+                <h3 class="maintitle maintitle--footer footer__primary__links__navigation__title">Navigation</h3>
+                <x-public.navigation.list.list-footer name_parent="footer__primary__links__navigation"/>
+            </div>
+            <div class="footer__primary__links__contact">
+                <h3 class="maintitle maintitle--footer footer__primary__links__contact__title">Contact</h3>
+                <x-utils.link name_parent="button--footer footer__primary__links__contact" href="mailto:amandine.fourny@student.hepl.be" label="amandine.fourny@student.hepl.be" title="Envoyer un mail à amandine.fourny@student.hepl.be"/>
+                <x-utils.link name_parent="button--footer footer__primary__links__contact" href="tel:0472483260" label="0472 48 32 60" title="Télephone au 0472 48 32 60"/>
+            </div>
         </div>
     </div>
     <div class="footer__secondary">
